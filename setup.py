5e054bee6025094d39a08b8c148f2dbcecacdc6f
David Graey <graeyband@gmail.com>
2023-03-13 20:48:28 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='package name',
+      version='0.0.1.dev1',
+      description='Package Template',
+      author='David Graey
+      author_email='graeyband@gmail.con',
+      url='',
+      packages=['pkg name'],
+     )

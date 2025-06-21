6a0b9e5fb626a58ea4871b4f47970839e8ea6b81
David Graey <graeyband@gmail.com>
2025-06-21 12:47:21 -0700

    updated optimizers and base model type

--- src/vehicles/nnets/optimizers.py
+++ src/vehicles/nnets/optimizers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy import NDArray
+from ..types import BasalModel
 import loss_functions as loss
 
 
@@ -103,3 +105,111 @@ def network_sgd_momentum(incoming_x, incoming_t,
             batch_error.append(error)
 
         error_trace.append(np.mean(batch_error))
+
+
+LAMBDA_MAX = 1e24
+LAMBDA_MIN =  1e-24
+# TODO: model with a calculate_gradient() func --
+def scaled_conjugate_gradient(model: BasalModel,
+                              x_data: NDArray,
+                              y_data: NDArray,
+                              iterations: int) -> NDArray:
+    """
+    This implementation is based on Charles Anderson's (Colorado State
+    University) SCG method --
+    It is a 'destructive' -- weights are adjusted as we take steps down the
+    gradient. Use with caution!
+    Requres a model object with specific methods implemented, hence the
+    BasalModel class.
+
+    Parameters
+    ----------
+    model : BasalModel
+    x_data :
+    y_data :
+    iterations :
+
+    Returns
+    -------
+
+    """
+    sigma_zero = 1e-6
+    lamb = 1e-6
+    lamb_ = 0
+
+    vector = model.get_weights().reshape(-1, 1)
+    grad_new, _ = model.calculate_gradients(x_data, y_data)
+    grad_new = -1 * grad_new.reshape(-1, 1)
+    r_new = grad_new.copy()
+    success = True
+
+    for _i in range(iterations):
+        r = r_new.copy()
+        grad = grad_new.copy()
+        mu = grad.T @ grad
+
+        if success:
+            success = False
+            sigma = sigma_zero / np.sqrt(mu)
+
+            grad_old, _ = model._calculate_gradients(x_data, y_data)
+            grad_old = grad_old.reshape(-1, 1)
+
+            # update our model's weights -- (take a step down the gradient)
+            model.weights = (vector + (sigma * grad)).reshape(model._weight_shape)
+            grad_step, _ = model._calculate_gradients(x_data, y_data)
+
+            step = (grad_old - grad_step.reshape(-1, 1)) / sigma
+            delta = grad.T @ step
+
+        # increase the curvature - "reach deeper"
+        zeta = lamb - lamb_
+        step += zeta * grad
+        delta += zeta * mu
+
+        if delta <= 0:
+            step += (lamb - 2 * delta / mu) * grad
+            lamb_ = 2 * (lamb - delta / mu)
+            delta -= lamb * mu
+            delta *= -1
+            lamb = lamb_
+
+        phi = grad.T @ r
+        alpha = phi / delta
+        vector_new = vector + alpha * grad
+        loss_old = model._calculate_loss(x_data, y_data)
+
+        # update our model's weights -- (take a step down the gradient)
+        model.weights = vector_new.copy().reshape(model._weight_shape)
+        loss_new = model._calculate_loss(x_data, y_data)
+
+        comparison = 2 * delta * (loss_old - loss_new) / (phi ** 2)
+
+        if comparison >= 0:
+            # break condition?
+            vector = vector_new.copy()
+            loss_old = loss_new
+
+            # update our model's weights -- (take a step down the gradient)
+            model.weights = vector_new.copy().reshape(model._weight_shape)
+            r_new, _ = model._calculate_gradients(x_data, y_data)
+            r_new = -1 * r_new.reshape(-1, 1)
+            success = True
+            lamb_ = 0
+
+            if _i % model._weight_shape[0] == 0:
+                grad_new = r_new
+            else:
+                beta = ((r_new.T @ r_new) - (r_new.T @ r)) / phi
+                # update our model's weights -- (take a step down the gradient)
+                grad_new = r_new + beta * grad
+
+            if comparison > 0.75:
+                lamb = max(0.5 * lamb, LAMBDA_MIN)
+        else:
+            lamb_ = lamb
+
+        if comparison < 0.25:
+            lamb = min(4 * lamb, LAMBDA_MAX)
+
+    return vector_new.reshape(model._weight_shape)
--- src/vehicles/types.py
+++ src/vehicles/types.py
@@ -2,11 +2,59 @@
 Typing complex objects & definitions for __package__
 """
 from enum import Enum, auto
+import numpy as np
+from abc import ABC, abstractmethod
 from typing import Protocol, Tuple, Dict, List, Optional, Union, Iterable, Callable
 from typing_extensions import TypedDict
 
+
+# -------------- Model Base Class  --------------
+class BasalModel(ABC):
+    def __init__(self,
+                 input_dimension: int,
+                 output_dimension: int,
+                 seed: int = 42):
+        self.RNG = np.random.default_rng(seed)
+
+        self.weights = np.empty()
+        self.weights_shape: tuple[int, ...] = ()
+
+        self.input_dimension = input_dimension
+        self.output_dimension = output_dimension
+        pass
+
+    @abstractmethod
+    def init_weights(self):
+       # set up initial weights
+       # assign self.weights_shape
+       pass
+
+    @abstractmethod
+    def forward(self):
+       pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+    @abstractmethod
+    def calculate_gradients(self):
+        pass
+
+    @abstractmethod
+    def calculate_loss(self):
+        pass
+
+    @property
+    def weights(self):
+        return self.weights[1:, ...]
+
+    @property
+    def weights(self):
+        return self.weights[1:, ...]
 
 # -------------- Complex typing  --------------
 class ComplexType(TypedDict):
     attribute_1: int
     # ...
+
